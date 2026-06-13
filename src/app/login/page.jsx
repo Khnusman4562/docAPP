242e05965bb21761bd99ce269a49236e3506b470
Usman Khan <khanusman4562@gmail.com>
2026-06-13 21:13:50 +0530

    adjust width

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,7 +1,7 @@
 export default function Login(){
     return(
-        <div className="flex justify-center items-center bg-green-200 h-[100vh]">
-            <div className="bg-white shadow-2xl p-8 text-center h-auto w-auto lg:h-[60%] lg:w-[22%] rounded-4xl flex flex-col gap-8">
+        <div className="flex justify-center items-center bg-green-200 h-[100vh] p-8">
+            <div className="bg-white shadow-2xl p-8 text-center h-auto w-full lg:h-[60%] lg:w-[22%] rounded-4xl flex flex-col gap-8">
                 <h2 className="text-4xl">Login</h2>
                 <input type="text" placeholder="Email" className="block border-2 border-gray-200 h-14 lg:h-20 px-4" />
                 <input type="password" placeholder="Enter your password" className="block border-2 border-gray-200 h-14 lg:h-20 px-4" />
